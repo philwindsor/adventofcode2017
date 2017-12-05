2dab19dd2158675aec33375cc25a223a807725be
Philip Windsor <philip.windsor@footy.com>
2017-12-05 16:26:58 +0000

    started project

---
+++ Day1/Day1/InverseCaptcha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017.Day1
+{
+    public class InverseCaptcha
+    {
+        public static int CalcSum(int number)
+        {
+
+        }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvdentOfCode2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
