640f1fcc75bb733fc4a145dcb8cbd799ba08b287
Philip Windsor <philip.windsor@footy.com>
2017-12-06 12:54:45 +0000

    day1 done!

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using AdventOfCode2017.Day1;
 
 namespace AdvdentOfCode2017
 {
@@ -6,7 +8,13 @@ namespace AdvdentOfCode2017
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine($"Day1 Answer: {InverseCaptcha.CalcSum()}");
+            Console.WriteLine($"Day1 Part2 Answer: {InverseCaptcha.CalcSumPart2()}");
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
