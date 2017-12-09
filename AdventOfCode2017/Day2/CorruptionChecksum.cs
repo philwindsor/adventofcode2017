8d349deaaa9829b9605bef8cba840abbf51aeb04
Phil <philipwindsor@outlook.com>
2017-12-09 21:34:19 +0000

    day2 done

--- AdventOfCode2017/Day2/CorruptionChecksum.cs
+++ AdventOfCode2017/Day2/CorruptionChecksum.cs
@@ -37,5 +37,47 @@ namespace AdventOfCode2017.Day2
 
             return checkSums.Sum();
         }
+
+        public static int CalculateCheckSum_Part2(string input)
+        {
+            var divisilbeSums = new List<int>();
+            var lineBreaks = new string[] { "\r\n", "\r", "\n" };
+            var colBreaks = new string[] { "\t", " " };
+            foreach (var row in input.Split(lineBreaks, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var cols = row.Split(colBreaks, StringSplitOptions.RemoveEmptyEntries);
+
+                var rowParsed = new int[cols.Length];
+
+                for (int i = 0; i < cols.Length; i++)
+                {
+                    rowParsed[i] = int.Parse(cols[i]);
+                }                
+
+                for (int i = 0; i < rowParsed.Length; i++)
+                {
+                    for (int j = 0; j < rowParsed.Length; j++)
+                    {
+                        if (i == j)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if( (rowParsed[i] % rowParsed[j]) == 0)
+                            {
+                                divisilbeSums.Add(rowParsed[i] > rowParsed[j] ? rowParsed[i] / rowParsed[j] : rowParsed[j] / rowParsed[i]);
+                                
+                                //exit loops
+                                j = rowParsed.Length + 1;
+                                i = rowParsed.Length + 1;
+                            }
+                        }
+                    }
+                }               
+            }
+
+            return divisilbeSums.Sum();
+        }
     }
 }
