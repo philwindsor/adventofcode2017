e92d440d0e93eae3ed8a379c7f85c7fc9d5af56f
Philip Windsor <philip.windsor@footy.com>
2017-12-15 17:20:48 +0000

    started day 3

---
+++ AdventOfCode2017/Day3/SpiralMemory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017.Day3
+{
+    public class SpiralMemory
+    {
+        public static int CalculateSteps(int number)
+        {
+            return 0;
+        }
+
+        public static int FindLowerSquareRoot(int number)
+        {
+            for (int i = number - 1; i >= 0; i--)
+            {
+                var result =  Math.Sqrt(number);
+
+                if((result % 2) == 0)
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception("Square Root Not Found");
+        }
+
+        public static int FindHigherSquareRoot(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                var result = Math.Sqrt(number);
+
+                if ((result % 2) == 0)
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception("Square Root Not Found");
+        }
+    }
+}
---
+++ Tests/Day1Tests/Day3Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using AdventOfCode2017.Day3;
+
+namespace AdventOfCodeTests
+{
+    public class Day3Tests
+    {
+        [Theory]
+        [InlineData(10, 9)]
+        public void FindLowerSquareRoot_Should_Return_Correct_Number(int num, int expected)
+        {
+            SpiralMemory.FindLowerSquareRoot(num).Should().Be(expected);
+        }
+    }
+}
